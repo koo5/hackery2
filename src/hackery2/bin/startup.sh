c4dad24e4b3ce5cd27fe102f05e8a3ab5cb814b9
Jindrich Kolman <kolman.jindrich@gmail.com>
2018-11-02 12:49:22 +0100

    ..

--- src/hackery2/bin/startup.sh
+++ src/hackery2/bin/startup.sh
@@ -3,6 +3,7 @@
 xfwm4 --replace &
 ~/hackery2/src/hackery2/bin/remap_numrow 
 bash -v ~/hackery2/src/hackery2/bin/update-yum 
+#sudo mount -t 9p -o trans=virtio,version=9p2000.L shared /shared
 ping 8.8.8.8
 cat
 
