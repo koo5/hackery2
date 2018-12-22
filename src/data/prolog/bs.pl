1d8f7459e158a6ed226c56e2b777568230c14fac
Your Name <you@example.com>
2018-12-22 01:22:50 +0100

    ...

--- src/data/prolog/bs.pl
+++ src/data/prolog/bs.pl
@@ -1,5 +1,8 @@
-bs(Ind) --> indents(Ind), [].
-bs(Ind) --> indents(Ind), "b:\n", bs(Ind).
+modules(Ind, Breadth, Depth) --> [];  {Breadth2 is Breadth - 1, dif(0, Breadth2)}, module(Ind, Breadth, Depth), modules(Ind, Breadth2, Depth).
+module(Ind, Breadth, Depth) --> "module:\n", {Ind2 is Ind + 1}, {Depth2 is Depth - 1, dif(0, Depth2)}, statements(Ind2, Breadth, Depth2).
+statements(Ind, Breadth, Depth) --> {Breadth2 is Breadth - 1, dif(0, Breadth2)}, statement(Ind, Breadth, Depth), statements(Ind, Breadth2, Depth); [].
+statement(Ind, Breadth, Depth) --> while(Ind, Breadth, Depth).
+while(Ind, Breadth, Depth) --> {Depth2 is Depth - 1, dif(0, Depth2)}, indents(Ind), "while true:\n", {Ind2 is Ind + 1}, statements(Ind2, Breadth, Depth2).
 
 indents(0) --> [].
 indents(I) --> {I > 0, J is I - 1}, "\t", indents(J).
@@ -9,10 +12,5 @@ putall([X|R]) :-
 	putall(R).
 
 putall([]).
-
-
-
-
-
 
 % phrase(bs(10), X),putall(X).
