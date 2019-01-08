ea0e50f29b64cbb8832cdd9227f589da0787bb21
Your Name <you@example.com>
2019-01-08 02:28:30 +0100

    mmm

--- src/hackery2/bin/startup.sh
+++ src/hackery2/bin/startup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #sudo systemctl restart spice-vdagent
 #sudo spice-vdagent -x -d
-xfwm4 --replace &
+#xfwm4 --replace &
 ~/hackery2/src/hackery2/bin/remap_numrow 
-bash -v ~/hackery2/src/hackery2/bin/update-yum 
+sudo ~/hackery2/src/hackery2/bin/update-yum 
 #sudo mount -t 9p -o trans=virtio,version=9p2000.L shared /shared
 ping 8.8.8.8
 cat
