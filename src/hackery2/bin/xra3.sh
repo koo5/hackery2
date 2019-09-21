4e85aa9b57f3f071bda8e9af6f8cfbc1f0790251
koo5 <kolman.jindrich@gmail.com>
2019-09-21 09:53:59 +0200

    ..

---
+++ src/hackery2/bin/xra3.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#xrandr --newmode "2560x1440_50.00"  256.25  2560 2736 3008 3456  1440 1443 1448 1484 -hsync +vsync
+
+
+xrandr --newmode "1920x2080_50.00"  281.00  1920 2064 2272 2624  2080 2083 2093 2142 -hsync +vsync
+
