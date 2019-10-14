b11b6be2f7e83d557c8f57680897e033f8949abb
Your Name <you@example.com>
2019-10-14 15:15:28 +0200

    mmm

--- src/hackery2/bin/backup_node.sh
+++ src/hackery2/bin/backup_node.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rsync -av --progress -h   -e 'ssh' -r  --exclude '.cache'  --exclude '__pycache__' --exclude 'sfi/.local/lib' --exclude 'sfi/.npm' --exclude 'site-packages' --exclude 'node_modules' sfi@51.140.155.227://home/ /home/koom/backups/`date "+%F-%H-%M-%S"`
+rsync -av --progress -h   -e 'ssh -p 44' -r  --exclude dont_backup --exclude venv --exclude '.cache'  --exclude '__pycache__'  --exclude '.npm'  --exclude 'site-packages'  --exclude "node_modules"  sfi@51.140.155.227://home/ ~/backups/(date '+%F-%H-%M-%S')
