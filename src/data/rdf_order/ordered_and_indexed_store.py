7ab49de15047d1c03a98607fdae69d53af7dd243
koo5 <kolman.jindrich@gmail.com>
2020-03-05 21:27:44 +0100

    ..

--- src/data/rdf_order/ordered_and_indexed_store.py
+++ src/data/rdf_order/ordered_and_indexed_store.py
@@ -1,142 +1,10 @@
-from collections import defaultdict
 from rdflib.store import Store
+from collections import defaultdict
 
-__all__ = ['OrderedStore', 'OrderedAndIndexedStore']
-
-
-class OrderedStore(Store):
-	context_aware = True
-	formula_aware = True
-	graph_aware = True
+__all__ = ['OrderedAndIndexedStore']
 
 
-	# Namespace persistence interface implementation
-	def bind(self, prefix, namespace):
-		self.nsBindings[prefix] = namespace
-
-	def prefix(self, namespace):
-		""" """
-		return dict(
-			[(v, k) for k, v in list(self.nsBindings.items())]
-		).get(namespace)
-
-	def namespace(self, prefix):
-		return self.nsBindings.get(prefix)
-
-	def namespaces(self):
-		for prefix, ns in list(self.nsBindings.items()):
-			yield prefix, ns
-
-
-	def __init__(self, configuration=None, identifier=None):
-		super(OrderedStore, self).__init__(configuration)
-		self.identifier = identifier
-		self.quads = []
-		self.quoted_info = []
-		self.__namespace = {}
-		self.__prefix = {}
-		self.nsBindings = {}
-		# ?
-		# default context information for triples
-		self.__defaultContexts = None
-
-	def __len__(s, context = None):
-		return sum(1 for _ in s.triples((None, None, None), context))
-
-
-	def copy(self):
-
-		r = self.__class__()
-		assert len(self.__namespace) == 0
-		assert len(self.__prefix) == 0
-		r.quads = self.quads[:]
-		r.quoted_info = self.quoted_info[:]
-		return r
-
-	def add(self, xxx_todo_changeme, context, quoted=False):
-		Store.add(self, xxx_todo_changeme, context, quoted)
-		s1, p1, o1 = xxx_todo_changeme
-		self.quads.append(((s1, p1, o1), context))
-		self.quoted_info.append(quoted)
-
-	"""
-	def bind(self, prefix, namespace):
-		self.__prefix[namespace] = prefix
-		self.__namespace[prefix] = namespace
-
-	def namespace(self, prefix):
-		return self.__namespace.get(prefix, None)
-
-	def prefix(self, namespace):
-		return self.__prefix.get(namespace, None)
-
-	def namespaces(self):
-		for prefix, namespace in self.__namespace.items():
-			yield prefix, namespace
-	"""
-
-	def remove(self, xxx_todo_changeme1, context=None):
-		# todo: figure out why the Memory store doesnt check the context
-		s1, p1, o1 = xxx_todo_changeme1
-		quads = self.quads[:]  # self.quads may be modified during the following iteration
-		for quad in quads:
-			if quad not in self.quads: continue
-			(s2, p2, o2), c2 = quad
-			if ((s1 == None) or (s1 == s2)) and ((p1 == None) or (p1 == p2)) and ((o1 == None) or (o1 == o2)) and (
-					context == c2):
-				i = self.quads.index(quad)
-				del self.quoted_info[i]
-				del self.quads[i]
-				self.remove(xxx_todo_changeme1, context)
-
-	# from collections import defaultdict
-	# stats = defaultdict(lambda : 0)
-
-	def triples(self, triplein, context=None):
-		"""
-		we try to optimize for the common pattern on (given subject, given predicate, return all objects, (in a given graph))
-		"""
-		# print("want ", triplein, " in ", context)
-		s1, p1, o1 = triplein
-
-		# self.__class__.stats["".join([('0' if (x == None) else '1') for x in (s1,p1,o1,context)])] += 1
-		# print (self.__class__.stats)
-
-		quads = self.quads[:]  # self.quads may be modified during the following iteration
-
-		if (s1 != None) and (p1 != None) and (o1 == None) and (context != None):
-			def filter_func(x):
-				(s2, p2, o2), c2 = x
-				return (s1 == s2) and (p1 == p2) and (context == c2)
-			return filter(filter_func, quads)
-
-		# return filter(lambda x: (s1 == x[0][0]) and (p1 == x[0][1]) and (context == x[1]), quads)
-
-		else:
-			return self.general_triple_helper(s1, p1, o1, context, quads)
-
-	#    for (s2,p2,o2),c2,_q2 in quads:
-	#        if (s1 == s2) and (p1 == p2) and (context == c2):
-	#            yield (s2,p2,o2),c2
-
-	def general_triple_helper(self, s1, p1, o1, context, quads):
-		for spo2, c2 in quads:
-			s2, p2, o2 = spo2
-			# print("trying", spo2,c2,_q2)
-			if ((s1 == None) or (s1 == s2)) and ((p1 == None) or (p1 == p2)) and ((o1 == None) or (o1 == o2)) and (
-					(context == None) or (context == c2)):
-				# print("matches")
-				yield spo2, c2
-
-	def contexts(self, triple=None):
-		if triple is None:
-			triple = (None,None,None)
-		yielded = []
-		for spo, c in self.triples(triple):
-			if c not in yielded:
-				yielded.append(c)
-				yield c
-
+"""OrderedStore has been removed, as it didn't seem useful and it was duplicate code. If controlling indexing is ever needed, extract the common functionality into a common class, or add a constructor parameter to enable/disble it at runtime"""
 
 
 patterns = ['ttft', 'fttt']
@@ -205,9 +73,6 @@ class OrderedAndIndexedStore(Store):
 	def namespaces(self):
 		for prefix, ns in list(self.nsBindings.items()):
 			yield prefix, ns
-
-
-
 
 
 	def copy(self):
