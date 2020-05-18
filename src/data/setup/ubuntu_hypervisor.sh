93a7f69eae69ee986a37adb079608db0c45756e1
Your Name <you@example.com>
2020-05-18 19:29:39 +0200

    ...

--- src/data/setup/ubuntu_hypervisor.sh
+++ src/data/setup/ubuntu_hypervisor.sh
@@ -2,11 +2,12 @@
 
 sudo apt update
 
-
-sudo apt install git
-
-
-
-
 sudo apt remove '^libreoffice.*'
+
+sudo apt install -y  mc htop fish tmux screen git iotop jnettop btrfs-progs build-essential libxrandr-dev ssh firewall-config fish arandr xcalib virt-manager gparted
+
+
+# sudo apt install kde-plasma-desktop
+# sudo apt install xubuntu-desktop
+
 
