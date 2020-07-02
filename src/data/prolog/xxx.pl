5bf3ad630781e5c1bed99d4ad21affbbe3c43e2d
koo5 <kolman.jindrich@gmail.com>
2020-07-02 15:06:29 +0200

    ..

--- src/data/prolog/xxx.pl
+++ src/data/prolog/xxx.pl
@@ -2,7 +2,10 @@
 :- use_module(library(clpfd)).
 
 x :-
-	print_term(a, []).
+	print_term(aaaaaaaaaaaa, []).
 
 count_ints(i(I), 1) :- I in inf..sup.
 count_ints(other(_), 0).
+
+%:- initialization((x)).
+%:- x.
