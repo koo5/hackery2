b84cd834fa7c72dea0bbdbbd73315bf3c21d1933
Your Name <you@example.com>
2020-12-04 09:35:10 +0100

    mmm

--- src/hackery2/bin/backup_node.sh
+++ src/hackery2/bin/backup_node.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env fish
 mkdir ~/backups
-rsync -av --progress -h   -e 'ssh -p 44' -r  --exclude dont_backup --exclude venv --exclude '.cache'  --exclude '__pycache__'  --exclude '.npm'  --exclude 'site-packages'  --exclude "node_modules"  --exclude "/home/sfi/ag/data" sfi@51.140.155.227://home/ ~/backups/(date '+%F-%H-%M-%S')
+#rsync -av --progress -h   -e 'ssh -p 44' -r  --exclude dont_backup --exclude venv --exclude '.cache'  --exclude '__pycache__'  --exclude '.npm'  --exclude 'site-packages'  --exclude "node_modules"  --exclude "/home/sfi/ag/data" sfi@51.140.155.227://home/ ~/backups/(date '+%F-%H-%M-%S')
+rsync -av --progress -h   -e 'ssh -p 44' -r  --exclude 'dont_backup'--exclude 'venv' --exclude '.cache'  --exclude '__pycache__'  --exclude '.npm'  --exclude 'site-packages'  --exclude "node_modules"  --exclude "/home/sfi/ag/data"      --include "/home/***" --include "/var/www/***" --include "/etc/***"    --exclude "*"   root@51.140.155.227:// ~/backups/(date '+%F-%H-%M-%S')
