4535180e796d718f3fba666b873ab29986a589e2
Your Name <you@example.com>
2021-09-15 14:54:37 +0200

    ..

--- src/hackery2/bin/hdp.py
+++ src/hackery2/bin/hdp.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
-import sys
+import sys,os
 import time
 import subprocess
-while True:
-	for i in 'abcde':
-		hdd = "/dev/sd" + i
-		print(subprocess.call("hdparm -C "+hdd + ";hdparm -y " + hdd + ";hdparm -C "+hdd, shell = True))
-	time.sleep(1)
+import click
+
+@click.command()
+@click.option('--keep_putting_to_sleep', type=bool, default=False)
+def main(keep_putting_to_sleep):
+	while True:
+		for i in 'abcde':
+			hdd = "/dev/sd" + i
+			print(hdd)
+			if not os.path.exists(hdd):
+				continue
+
+			print(subprocess.call("hdparm -C "+hdd, shell = True))
+			if keep_putting_to_sleep:
+				print(subprocess.call("hdparm -y " + hdd, shell = True))
+				print(subprocess.call("hdparm -C "+hdd, shell = True))
+	
+			time.sleep(1)
+if __name__ == '__main__':
+	main()
--- src/hackery2/bin/xfwmloop.py
+++ src/hackery2/bin/xfwmloop.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 import os, time, sys
 while True:
-	os.system('fish -c "env DISPLAY=:0.0 xfwm4 --replace"')
+	try:
+		os.system('fish -c "env DISPLAY=:0.0 xfwm4 --replace"')
+	except:
+		pass
 	time.sleep(1)
