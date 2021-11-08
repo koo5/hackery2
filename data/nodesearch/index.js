5981afefddf6d5fbd911935cd67fb8b478445bdb
koo5 <kolman.jindrich@gmail.com>
2021-11-08 04:45:57 +0100

    ..

--- data/nodesearch/index.js
+++ data/nodesearch/index.js
@@ -3,7 +3,7 @@
 const fs = require('fs')
 var parse = require('dotparser');
 var ast = parse(fs.readFileSync('test.gv', 'utf8'));
-console.log(ast);
+//console.log(ast);
 let g = ast[0].children
 let nodepairs = {};
 g.forEach((stmt) =>
@@ -21,10 +21,14 @@ g.forEach((stmt) =>
 				let pair = JSON.stringify({'src': node0, 'tgt': e});
 				//console.log(pair)
 				if (nodepairs.hasOwnProperty(pair))
+				{
+					console.log('duplicate edge:');
 					console.log(pair);
+				}
 				nodepairs[pair] = true;
 			}
 			node0 = e;
 		})
 	}
 });
+console.log(`${Object.keys(nodepairs).length} edges found.`);
