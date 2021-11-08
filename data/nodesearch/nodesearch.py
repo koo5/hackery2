c18bb03b7378cd9b9f6ab0baaee5b8b4bc0a8b93
Your Name <you@example.com>
2021-11-08 23:04:48 +0100

    ..

--- data/nodesearch/nodesearch.py
+++ data/nodesearch/nodesearch.py
@@ -1,8 +1,10 @@
+#pydot seems to not load the file correctly and some edges dont have a target
 
 
 import pydot
 graphs = pydot.graph_from_dot_file('test.gv')
 print(graphs)
+
 g = graphs[0]
 # for e in g.edges:
 # 	pair = (e.source,e.target)
---
+++ notes/CO2.md
@@ -0,0 +1,3 @@
+https://automatizace.hw.cz/kvalita-vzduchu-v-uzavrenych-mistnostech-7-co2.html
+https://www.co2levels.org/
+
