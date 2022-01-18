04a6f3f5b951b4019cc46476410fb7156e77ef42
Your Name <you@example.com>
2022-01-18 22:47:08 +0100

    ..

--- data/luks_integrity/settings.sh
+++ data/luks_integrity/settings.sh
@@ -1,8 +1,16 @@
 
+export WORKDIR=/run/luks_integrity_benchmark
+# where to mount ramfs
 export MNT=$WORKDIR/ramfs
 mkdir -p $MNT
+# image file path
 export DEV=$MNT/image.raw
-
+# crypto key
+export KEY=$WORKDIR/key
+# dev-mapper device name
+export CRYPTDEV=luks_integrity_benchmark1
+# dd
+export DD="dd status=progress"
 
 # block size that we will write/read
 export BS=4096
@@ -14,9 +22,4 @@ export BC=20000000
 # how much data should we actually try to read and write, this is better to be lower than the image size
 export BCDATA=$(python3 -c "import os;print(int(round(int(os.environ['BC']) / 5 * 4)))")
 
-export MP=$WORKDIR/luks
-mkdir -p MP
-export CRYPTDEV=luks_integrity_benchmark1
-
-export DD="dd status=progress"
 
--- data/luks_integrity/try_cyp.sh
+++ data/luks_integrity/try_cyp.sh
@@ -12,21 +12,25 @@ dd status=none if=/dev/zero bs=4096 count=10 of=$DEV  conv=notrunc
 echo
 echo "formatting with luks2  $CYP  ..."
 echo "YES" | sh -c "cryptsetup --key-file  key  luksFormat --type luks2    $CYP   $DEV "
-sync
+sync; sleep 10
+
 cryptsetup --key-file  key   open   $DEV $CRYPTDEV
+sync; sleep 10
 
 echo "writing  ..."
 sh -c "$DD if=/dev/zero bs=$BS count=$BCDATA of=/dev/mapper/$CRYPTDEV"
-sync
+sync; sleep 10
 uptime
 cryptsetup close $CRYPTDEV
 
 echo "reading it back:"
-sync
+sync; sleep 10
 cryptsetup --key-file  key  open   $DEV  $CRYPTDEV
-sync
+sync; sleep 10
+
 sh -c "$DD if=/dev/mapper/$CRYPTDEV bs=$BS count=$BCDATA of=/dev/null"
-sync
+sync; sleep 10
+
 uptime
 cryptsetup close $CRYPTDEV
 
