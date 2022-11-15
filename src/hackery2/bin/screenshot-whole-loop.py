a6b0c89379512e0ae43f4944955457ef0b6e20c7
Jindrich Kolman <kolman.jindrich@gmail.com>
2022-11-15 20:02:04 +0100

    ..

--- src/hackery2/bin/screenshot-whole-loop.py
+++ src/hackery2/bin/screenshot-whole-loop.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python3
-
-
-
-
-
 import os, time, sys, shutil
 from datetime import datetime
 
@@ -19,8 +14,6 @@ def print_free_space_very_smartly():
 	i += 1
 	if i > 10:
 		i = 0
-
-
 
 
 # the arguments to the script are: folder, delay, scrot args
@@ -47,3 +40,4 @@ while True:
 
 # generate video with crop:
 # ffmpeg -framerate 1 -pattern_type glob -i '*.png'   -c:v libx264 -pix_fmt yuv420p -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -vf "crop=1920:1080:0:0"   (date --utc "+%Y-%m-%d_%H-%M-%S.%N").mp4
+
