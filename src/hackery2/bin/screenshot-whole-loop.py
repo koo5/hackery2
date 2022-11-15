4917123d280e7141adccaf209c23791f48442bab
Jindrich Kolman <kolman.jindrich@gmail.com>
2022-11-15 20:06:38 +0100

    ..

--- src/hackery2/bin/screenshot-whole-loop.py
+++ src/hackery2/bin/screenshot-whole-loop.py
@@ -3,37 +3,38 @@ import os, time, sys, shutil
 from datetime import datetime
 
 
-i = 0
-
-def print_free_space_very_smartly():
-	global i
-	if i == 0:
-		os.system("df -h " + sys.argv[1])
-	#else:
-	#	os.system("df -h " + sys.argv[1] + " | tail -n 1")
-	i += 1
-	if i > 10:
-		i = 0
-
-
 # the arguments to the script are: folder, delay, scrot args
+# like this: screenshot-whole-loop.py ~/screenshots 1 -f
+
+dest_dir = os.path.abspath(sys.argv[1])
+
 
 tmp_fn = '/tmp/screenshot-whole-loop.png'
-dest_dir = os.path.abspath(sys.argv[1])
 try:
 	os.makedirs(dest_dir)
 except FileExistsError:
 	pass
 
+
+i = 0
+def print_free_space_very_smartly():
+	global i
+	if i == 0:
+		os.system("df -h " + dest_dir)
+	i += 1
+	if i == 10:
+		i = 0
+
+
 while True:
+	print_free_space_very_smartly()
 	dest_fn = dest_dir + "/" + datetime.now().strftime("%Y_%m_%d_%H_%M_%S.%f") + ".png"
-	print(dest_fn)
 	cmd = "scrot  -q 0  " +sys.argv[3]+ " " + tmp_fn
-	print(cmd)
+	#print(cmd)
 	os.system(cmd)
 	shutil.move(tmp_fn, dest_fn)	
+	print(dest_fn)
 	#os.system('geeqie -r --first')
-	print_free_space_very_smartly()
 	time.sleep(int(sys.argv[2]))
 
 
