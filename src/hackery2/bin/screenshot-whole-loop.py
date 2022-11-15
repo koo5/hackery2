ea9a0e51fa02913c8bc2c8b29abca1e98d751220
Jindrich Kolman <kolman.jindrich@gmail.com>
2022-11-15 20:00:42 +0100

    ..

--- src/hackery2/bin/screenshot-whole-loop.py
+++ src/hackery2/bin/screenshot-whole-loop.py
@@ -1,24 +1,49 @@
 #!/usr/bin/env python3
-import os, time, sys
+
+
+
+
+
+import os, time, sys, shutil
+from datetime import datetime
+
+
 i = 0
-# folder, delay, scrot args
-while True:
-	os.system("scrot  -q 0  " +sys.argv[3]+ " " +  sys.argv[1]+"/$(date +%Y%m%d%H%M%S).png")
-#	os.system('geeqie -r --first')
 
-
-
-	# print free space, very smartly
-
+def print_free_space_very_smartly():
+	global i
 	if i == 0:
 		os.system("df -h " + sys.argv[1])
-	else:
-		os.system("df -h " + sys.argv[1] + " | tail -n 1")
+	#else:
+	#	os.system("df -h " + sys.argv[1] + " | tail -n 1")
 	i += 1
 	if i > 10:
 		i = 0
 
 
 
+
+# the arguments to the script are: folder, delay, scrot args
+
+tmp_fn = '/tmp/screenshot-whole-loop.png'
+dest_dir = os.path.abspath(sys.argv[1])
+try:
+	os.makedirs(dest_dir)
+except FileExistsError:
+	pass
+
+while True:
+	dest_fn = dest_dir + "/" + datetime.now().strftime("%Y_%m_%d_%H_%M_%S.%f") + ".png"
+	print(dest_fn)
+	cmd = "scrot  -q 0  " +sys.argv[3]+ " " + tmp_fn
+	print(cmd)
+	os.system(cmd)
+	shutil.move(tmp_fn, dest_fn)	
+	#os.system('geeqie -r --first')
+	print_free_space_very_smartly()
 	time.sleep(int(sys.argv[2]))
 
+
+
+# generate video with crop:
+# ffmpeg -framerate 1 -pattern_type glob -i '*.png'   -c:v libx264 -pix_fmt yuv420p -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -vf "crop=1920:1080:0:0"   (date --utc "+%Y-%m-%d_%H-%M-%S.%N").mp4
