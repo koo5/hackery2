b70ab744762c6888f52e10468cf35e5ff7257ced
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-02-12 16:42:20 +0100

    ..

--- data/setup/data/fish/functions/gclur.fish
+++ data/setup/data/fish/functions/gclur.fish
@@ -1,5 +1,8 @@
 function gclur
-	set r (clone_into_username.py $argv)
-	echo 'got:' $r
-	cd (echo $r | jq -r '.filesystem_path')
+	set tmp (mktemp)
+	ls /etc/xxx
+	lone_into_username.py $argv | tee $tmp
+	set result (cat tmp | tail -n 1)
+	echo 'gclur.fish got:' $result
+	cd (echo $result | jq -r '.repo_fs_path')
 end
--- src/hackery2/bin/clone_into_username.py
+++ src/hackery2/bin/clone_into_username.py
@@ -1,21 +1,47 @@
 #!/usr/bin/env python3
 
 
-import logging,os,sys,subprocess,time,shlex,logging, pathlib, urllib, json
+import logging,os,sys,subprocess,time,shlex,logging, pathlib, urllib, json, click
+logger = logging.getLogger('ciu')
 
-urlstr = sys.argv[1]
-optional_clone_parameters = sys.argv[1:-1]
 
-url = urllib.parse.urlparse(urlstr)
-logging.warning(str(url))
-path = url.path.strip('/').split(':')[-1].split('/')
-user = path[0]
-proj = path[1].removesuffix('.git')
-d = os.path.expanduser('~/repos/'+user+'/'+proj+'/0/')
 
-pathlib.Path(d).mkdir(exist_ok=True,parents=True)
-os.chdir(d)
-print(json.dumps({'filesystem_path': d + proj}))
+@click.command()
+@click.argument('url', nargs=1)
+@click.argument('optional_clone_args', nargs=-1)
+def clone(url, optional_clone_args):
+	url_parsed = urllib.parse.urlparse(url)
+	logger.info(str(url_parsed))
 
-cmd = ['git', 'clone', '--recurse-submodules'] + optional_clone_parameters + [urlstr]
-sys.exit(subprocess.call(cmd))
+	# taking the right side of a possible comma makes it handle both a https schema and a ssh schema
+	path_cleaned = url_parsed.path.strip('/').split(':')[-1]
+	logger.info('path_cleaned: ' + path_cleaned.__repr__())
+
+	path = path_cleaned.split('/')
+	logger.info('path: ' + path.__repr__())
+
+	user = path[0]
+	proj = path[1].removesuffix('.git')
+	logger.info('user: ' + user.__repr__())
+	logger.info('proj: ' + proj.__repr__())
+
+
+	parent_fs_path = os.path.expanduser('~/repos/'+user+'/'+proj+'/0/')
+	logger.info('parent_dir: ' + parent_fs_path.__repr__())
+
+	pathlib.Path(parent_fs_path).mkdir(exist_ok=True,parents=True)
+	os.chdir(parent_fs_path)
+
+	repo_fs_path = parent_fs_path + proj
+	print('\n' + json.dumps({'repo_fs_path': repo_fs_path}))
+
+	git_cmd = ['git', 'clone', '--recurse-submodules'] + list(optional_clone_args) + [url]
+	logger.info('git_cmd: ' + shlex.join(git_cmd))
+
+	sys.exit(subprocess.call(git_cmd))
+
+
+
+if __name__ == '__main__':
+	logging.basicConfig(level="INFO")
+	clone()
