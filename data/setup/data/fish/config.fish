e4854fc22d53ca051f2579083c1c9f0dd0ae525c
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-04-10 01:31:39 +0200

    ..

---
+++ data/setup/data/fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive  # Commands to run in interactive sessions can go here
+  . /home/koom/hackery2/data/setup/data/fish/functions/auto_venv.fish
+  __auto_source_venv
+  . /home/koom/hackery2/data/setup/data/fish/aaaa.fish
+end
+
+
+#starship init fish | source
+
+
