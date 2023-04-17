c10aaa2e2ed4540ec52aac8613321f24c643852d
Your Name <you@example.com>
2023-04-18 00:14:06 +0200

    ..

--- src/hackery2/bin/disks.pl
+++ src/hackery2/bin/disks.pl
@@ -37,7 +37,7 @@ stop(I) :-
 	shell2(['cryptdisks_stop bac', I]),
 	shell2(['cryptdisks_stop bac', I, '_2']).
 
-my_disks_are([0,1,2,3,4,5,6,7,8,9,10,11]).
+my_disks_are([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]).
 
 start :-
 	my_disks_are(Disks),
