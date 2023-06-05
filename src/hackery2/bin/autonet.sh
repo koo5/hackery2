aaa72cafa3892fa00663e3fdd65ff7a84b6c0807
Your Name <you@example.com>
2023-06-05 12:59:11 +0200

    autonet

--- src/hackery2/bin/autonet.sh
+++ src/hackery2/bin/autonet.sh
@@ -4,7 +4,7 @@ ping 8.8.8.8 -c 1; and exit 0
 
 
 sudo dhcping -s 192.168.8.1; and sudo nmcli c up 'strasnice drat!'
-sudo dhcping -s 10.0.0.138; and sudo nmcli c up 'prosek drat'
+sudo dhcping -s 10.0.0.138  -h "34:e6:d7:0f:a9:83"; and sudo nmcli c up 'prosek drat'
 
 
 
