991daead69c08f54db7dc184fa740069d0fae226
Your Name <you@example.com>
2023-06-06 16:13:17 +0200

    ..

--- data/setup/data/fish/config.fish
+++ data/setup/data/fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive  # Commands to run in interactive sessions can go here
   . /home/koom/hackery2/data/setup/data/fish/functions/auto_venv.fish
   __auto_source_venv
-  . /home/koom/hackery2/data/setup/data/fish/aaaa.fish
+  . /home/koom/hackery2/data/setup/data/fish/functions/aaaa.fish
 end
 
 
