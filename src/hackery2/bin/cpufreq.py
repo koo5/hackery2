0dd5866bb6d148254203e3ba24ad882c50134f34
Your Name <you@example.com>
2023-06-19 11:46:32 +0200

    ..

--- src/hackery2/bin/cpufreq.py
+++ src/hackery2/bin/cpufreq.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
+import os
 import sys
 import time
 import shlex
 import subprocess
-cmd = sys.argv[1:]
+cmd = sys.argv[2:]
 
-for i in range(24):
+for i in range(int(sys.argv[1])):
 	cmdlist = shlex.split("cpufreq-set -c " + str(i))
 	cmdlist += cmd
 	print(shlex.join(cmdlist))
 	subprocess.call(cmdlist)
-#	time.sleep(0.1)
+	#	time.sleep(int(os.environ['DELAY']))
 
