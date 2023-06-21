d01107512c0db44cde2b1ef8ce8ed58a6779af0b
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-06-21 18:28:41 +0200

    ..

--- src/hackery2/bin/sleep.sh
+++ src/hackery2/bin/sleep.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env fish
+set -x
 set DISPLAY :0 
 screen_off.sh
-sudo rmmod -vvvv aquantia; sudo rmmod -vvvv atlantic; sleep 5; all_usb_wakeup.sh disabled; sudo  bash -c " echo mem > /sys/power/state" ; sleep 10;  sudo modprobe -vvvv atlantic;  sudo dmesg --follow;
+sudo rmmod -vvvv aquantia;
+sudo rmmod -vvvv atlantic;
+sleep 5;
+all_usb_wakeup.sh disabled;
+sudo bash -c " echo mem > /sys/power/state" ;
+sleep 10;
+sudo modprobe -vvvv atlantic;
+sudo dmesg --follow;
+
+
 
