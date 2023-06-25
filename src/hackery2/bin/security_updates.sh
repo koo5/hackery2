06c1d2e54ffad38d5577f76a50174f260a4018b6
Your Name <you@example.com>
2023-06-25 16:38:46 +0200

    security_updates.sh

--- src/hackery2/bin/security_updates.sh
+++ src/hackery2/bin/security_updates.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env fish
 grep -h security -R /etc/apt/sources.list /etc/apt/sources.list.d/ | sudo tee /etc/apt/sec
 and sudo apt-get upgrade -o Dir::Etc::SourceParts='' -o Dir::Etc::SourceList=/etc/apt/sec
+
+# https://serverfault.com/a/282518/98242
+# https://youtu.be/fo8zpBm05Dc
