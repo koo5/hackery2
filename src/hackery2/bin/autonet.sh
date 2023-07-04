ef4f4093a3f93153904d82dc7ad0141380901806
Your Name <you@example.com>
2023-07-04 21:57:51 +0200

    ..

--- src/hackery2/bin/autonet.sh
+++ src/hackery2/bin/autonet.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env fish
 
-ping 8.8.8.8 -c 1; and exit 0
+#ping 8.8.8.8 -c 1; and exit 0
+
+sudo nmcli c show 'strasnice drat!' | grep GENERAL.STATE: | grep activated; and exit 0;
+sudo nmcli c show 'prosek drat'     | grep GENERAL.STATE: | grep activated; and exit 0;
 
 sudo dhclient -v enp0s25
-
 sudo dhcping -s    192.168.8.1   -h "a4:5d:36:9b:18:16";   and sudo nmcli c up 'strasnice drat!'
 sudo dhcping -s    10.0.0.138    -h "a4:5d:36:9b:18:16";   and sudo nmcli c up 'prosek drat'
 
--- src/hackery2/bin/security_updates.sh
+++ src/hackery2/bin/security_updates.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env fish
-grep -h security -R /etc/apt/sources.list /etc/apt/sources.list.d/ | sudo tee /etc/apt/sec
-and sudo apt-get update -o Dir::Etc::SourceParts='' -o Dir::Etc::SourceList=/etc/apt/sec
-and sudo apt-get upgrade -o Dir::Etc::SourceParts='' -o Dir::Etc::SourceList=/etc/apt/sec
+
+
+sudo aptdcon --fix-install
+and grep -h security -R /etc/apt/sources.list /etc/apt/sources.list.d/ | sudo tee /etc/apt/sec
+and sudo apt-get update  -o Dir::Etc::SourceParts='' -o Dir::Etc::SourceList=/etc/apt/sec  --error-on=any 
+and sudo apt-get upgrade -o Dir::Etc::SourceParts='' -o Dir::Etc::SourceList=/etc/apt/sec  -y --allow-downgrades
+
 
 # https://serverfault.com/a/282518/98242
 # https://youtu.be/fo8zpBm05Dc
