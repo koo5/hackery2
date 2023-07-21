9b216c885a8274c74270acb244ddce144a3c1e91
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-07-21 18:44:01 +0200

    ...

--- src/hackery2/bin/backup.py
+++ src/hackery2/bin/backup.py
@@ -32,20 +32,29 @@ def run(local=False, target='/bac4/'):
 	# toplevel is where the bulk of the data lives, divided into subvolumes
 
 	if hostname == 'hp':
-		subvols = ['home', 'backup','lean','leanpriv']
-		toplevel = '/mx500data'
+		fss = [{
+			'toplevel': '/mx500data',
+			'subvols': ['backup', 'home', 'lean','leanpriv'],
+		}]
 	elif hostname == 'jj':
-		subvols = ['backup','images','images2','u']
-		toplevel = '/d2'
+		fss = [{
+			'toplevel': '/d2',
+			'subvols': ['backup','images','images2','u'],
+		}]
 	elif hostname == 'r64':
-		subvols = ['/']
-		toplevel = '/'
+		fss = [{
+			'toplevel': '/',
+			'subvols': ['/'],
+		},{
+			'toplevel': '/bac4',
+			'subvols': ['lean', 'images_win'],
+		}]
 
 
 
 	# rsync ext4 filesystems into btrfs backup/ folder
 
-	where = f'{toplevel}/backup/root_ext4'
+	where = f"{fss[0]['toplevel']}/backup/root_ext4"
 	if hostname == 'hp':
 		rsync('/boot /root /etc /var/www /var/lib/docker/volumes', where)
 	elif hostname == 'jj':
@@ -54,9 +63,11 @@ def run(local=False, target='/bac4/'):
 	
 	# transfer btrfs subvolumes
 
-	for subvol in subvols:
-		target_subvol_name = subvol if subvol != '/' else '_root'
-		ccs(f"""bfg --YES=true {sshstr} --LOCAL_FS_TOP_LEVEL_SUBVOL_MOUNT_POINT={toplevel} commit_and_push_and_checkout 			--SUBVOLUME={toplevel}/{subvol}/ --REMOTE_SUBVOLUME=/{target}/backup_{hostname}/{target_subvol_name}""")
+	for fs in fss:
+		toplevel = fs['toplevel']
+		for subvol in fs['subvols']:
+			target_subvol_name = subvol if subvol != '/' else '_root'
+			ccs(f"""bfg --YES=true {sshstr} --LOCAL_FS_TOP_LEVEL_SUBVOL_MOUNT_POINT={toplevel} commit_and_push_and_checkout 			--SUBVOLUME={toplevel}/{subvol}/ --REMOTE_SUBVOLUME=/{target}/backup_{hostname}/{target_subvol_name}""")
 
 
 def rsync(what,where):
@@ -65,3 +76,16 @@ def rsync(what,where):
 
 if __name__ == "__main__":
 	fire.Fire(run)
+
+
+
+"""
+maybe we can make bfg smart enough to "detect" the filesystem that a given subvol resides in, and in turn
+parse /etc/mtab to find if the filesystem is mounted with subvol=. If not, no "toplevel" parameter is needed.
+Otherwise, it'd be prompted/required, or it could be assumed from other mtab lines.	
+	
+	
+	
+	
+	
+"""
