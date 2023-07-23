a1f2da396d2688d7477cfdb1152d7c1771ed9007
Your Name <you@example.com>
2023-07-23 13:04:09 +0200

    ..

--- data/setup/data/fish/autorecord/autorecord.fish
+++ data/setup/data/fish/autorecord/autorecord.fish
@@ -10,10 +10,12 @@ if status is-interactive
 		env > $my-env
 
 		set ts (date -u '+%Y-%m-%dZ%H-%M-%S-%N')
+
+		# this variable will prevent the fish spawned on the next line to try to spawn another fish ad infinity
 		set -gx SESSION_RECORD $my-session
 
 		# User session happens inside the script command here, it invokes another instance of fish
-		script -q -f -e -T $SESSION_RECORD.timing -B $SESSION_RECORD
+		script -q --flush -e -T $SESSION_RECORD.timing -B $SESSION_RECORD
 
 		set exit_status $status
 		echo "exit $exit_status"
@@ -24,7 +26,9 @@ if status is-interactive
 		end
 
 	else
+		#SESSION_RECORD is set, this means we are the fish invoked from within script
 		echo scriptreplay -t $SESSION_RECORD.timing -B $SESSION_RECORD --maxdelay 0.5 --divisor 1
 	end
+	# we erase this before we get to the interactive shell, so that nested invocations of fish can do their own recording too
 	set --erase SESSION_RECORD
 end
