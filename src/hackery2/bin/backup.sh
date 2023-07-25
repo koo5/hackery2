364a5fdea517db25456ab15c745c17ae2ee5a4d7
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-07-25 07:36:18 +0200

    ..

--- src/hackery2/bin/backup.sh
+++ src/hackery2/bin/backup.sh
@@ -3,4 +3,5 @@
 cd (dirname (readlink -m (status --current-filename)))
 set out ~/"backup_log_utc_"(date -u '+%Y-%m-%d_%H-%M-%S')
 PYTHONUNBUFFERED=1 python3 ./backup.py $argv | tee $out
-cat out | grep \{\"result
+echo '------'
+cat $out | grep \{\"result
