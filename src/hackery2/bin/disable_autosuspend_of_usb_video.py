daac8ef284a6f981a615d5271ac814e337920806
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-09-07 08:25:19 +0200

    ..

--- src/hackery2/bin/disable_autosuspend_of_usb_video.py
+++ src/hackery2/bin/disable_autosuspend_of_usb_video.py
@@ -1,31 +1,38 @@
 #!/usr/bin/env python3
 
 
-import os,time,datetime,subprocess,shlex,glob,pathlib
+import os,time,datetime,subprocess,shlex,glob,pathlib,sys,click
 
 
 
 
-def toggle(f):
+def toggle(f,new_state):
 	f = pathlib.Path(f + '/../power/control').resolve(strict=False)
-	print(str(f) + ' << on')
-	open(f,'w').write('on')
+	print(str(f) + ' << ' + new_state)
+	open(f,'w').write(new_state)
 	print()
 
 
+@click.command()
+@click.argument('new_state', type=str, 	default='on')
+def x(new_state):
+
+	for f in glob.glob('/sys/bus/usb/devices/*/*'):
+		try:
+			maybe_interface_file = f + '/interface'
+
+			with open(maybe_interface_file) as i:
+				contents = i.readlines()[0]
+				#print(maybe_interface_file)
+				#print(contents.__repr__())
+				if contents == 'USB Video\n':
+					#print('toggle')
+					toggle(f,new_state)
+		except (NotADirectoryError, FileNotFoundError) as e:
+			#print(e.__repr__())
+			pass
 
 
-for f in glob.glob('/sys/bus/usb/devices/*/*'):
-	try:
-		maybe_interface_file = f + '/interface'
 
-		with open(maybe_interface_file) as i:
-			contents = i.readlines()[0]
-			#print(maybe_interface_file)
-			#print(contents.__repr__())
-			if contents == 'USB Video\n':
-				#print('toggle')
-				toggle(f)
-	except (NotADirectoryError, FileNotFoundError) as e:
-		#print(e.__repr__())
-		pass
+if __name__ == '__main__':
+	x()
