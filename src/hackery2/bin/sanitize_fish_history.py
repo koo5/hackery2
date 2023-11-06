980290b0b8a9088eb24983fcf194bc8b3eff3936
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-11-06 13:52:29 +0100

    ..

---
+++ src/hackery2/bin/sanitize_fish_history.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+import os,time,datetime
+from os.path import expanduser
+
+
+def lineafter(line, param):
+	if line.startswith(param):
+		return line[len(param):]
+
+
+def fish_history_entries(fish_history_file_name = '~/.local/share/fish/fish_history'):
+	fish_src = expanduser(fish_history_file_name)
+	print(f"Reading '{fish_src}' ...")
+	
+	cmd = None
+	
+	with open(fish_src, "r") as fs:
+
+		try:
+			line = next(fs)
+			while True:
+	
+				cmd   = lineafter(line, '- cmd: ')
+				if cmd is None:
+					raise Exception('hmm')
+				line = next(fs)
+	
+				when  = lineafter(line, '  when: ')
+				if when is None:
+					raise Exception('hmm')
+				line = next(fs)
+	
+				paths = lineafter(line, '  paths:')
+				if paths is not None:
+					paths_entries = []
+					while True:
+						line = next(fs)
+						if line.startswith('- cmd: '):
+							break
+						paths_entries.append(lineafter(line, '    - ').strip())
+				
+				yield dict(cmd=cmd.strip(), when=when.strip(), paths=paths_entries if paths is not None else None)
+		except StopIteration:
+			pass
+def is_sane(entry):
+	for bad_word in 'mqtt mosquitto'.split():
+		#print(f"""{bad_word} in {entry['cmd']}?""")
+		if bad_word in entry['cmd']:
+			print(f"Found bad word '{bad_word}' in '{entry['cmd']}'")
+			print(f"Removing entry '{entry['cmd']}'")
+			return False
+	return True
+
+
+
+def write_history(new_history, fn):
+	with open(expanduser(fn), 'w') as f:
+		for e in new_history:
+			f.write('- cmd: ' + e["cmd"] + '\n')
+			f.write(f'  when: {e["when"]}\n')
+			if e["paths"]:
+				f.write(f'  paths:\n')
+				for path in e["paths"]:
+					f.write(f'    - {path}\n')
+
+def sanitize():
+	num_sane = 0
+	num_insane = 0
+	new_history = []
+	refuse = []
+	for entry in fish_history_entries():
+		print(entry)
+		sane = is_sane(entry)
+		if sane:
+			num_sane += 1
+		else:
+			num_insane += 1
+		if sane:
+			new_history.append(entry)
+		else:
+			refuse.append(entry)
+	write_history(new_history, '~/.local/share/fish/sane_history')
+	# new file name with timestamp with nonosec precision:
+	insane_history_fn = '~/.local/share/fish/insane_history_' + datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S.%f')
+	write_history(refuse, insane_history_fn)
+	print(f"wrote {num_sane} sane entries and dropped {num_insane} insane entries")
+
+sanitize()
