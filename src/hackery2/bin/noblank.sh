235695d5ac0b6bcb27b77198da1acd3124345636
Your Name <you@example.com>
2023-12-22 15:54:37 +0100

    ..

--- src/hackery2/bin/noblank.sh
+++ src/hackery2/bin/noblank.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env fish
 xset -dpms s off s noblank s 0 0 s noexpose
+xset  s off s noblank s 0 0 s noexpose
 gsettings set org.gnome.desktop.session idle-delay 0
 gsettings set org.gnome.desktop.lockdown disable-lock-screen 'true'
 
