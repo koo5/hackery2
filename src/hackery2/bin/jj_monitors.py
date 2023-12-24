9c2770dcadcbad7101f795636b2c16d65fd9062b
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-12-24 13:31:51 +0100

    ..

--- src/hackery2/bin/jj_monitors.py
+++ src/hackery2/bin/jj_monitors.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python3
 
 
-import os,time,datetime,subprocess,glob
-
-for x in """
+import os
 
 
-
-""".splitlines():
-os.system('xrandr ' + x)
-
-
-'xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output HDMI-A-0 --primary --mode 3840x2160 --pos 1920x1080 --rotate normal --output DVI-D-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-I-4-4 --off --output DVI-I-3-3 --off --output DVI-I-2-2 --off --output DVI-I-1-1 --mode 1280x1024 --pos 5760x3240 --rotate normal
+os.system('/home/koom/hackery2/data/xrandr/xrandr.py ~/.screenlayout/fff.sh')
 
 
 
-a bit of a problem is that we can't detect if a tv is switched on or off
-i guess the only solution is to hack arandr to:
-	1) write proper settings scriipts :)
-	2) periodically write current configured configuration to a file somewhere, where a periodic script can grab it, in a declarative way,
-
-	iiidk
+# 
+# a bit of a problem is that we can't detect if a tv is switched on or off
+# i guess the only solution is to hack arandr to:
+# 	1) write proper settings scriipts :)
+# 	2) periodically write current configured configuration to a file somewhere, where a periodic script can grab it, in a declarative way,
+# 
+# 	iiidk
--- src/hackery2/bin/xrandr.py
+++ src/hackery2/bin/xrandr.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # sudo apt install python3-xlib
 
 from Xlib import X, display
@@ -8,7 +10,9 @@ s = d.screen()
 window = s.root.create_window(0, 0, 1, 1, 1, s.root_depth)
 
 res = randr.get_screen_resources(window)
-import IPython; IPython.embed()
+#import IPython; IPython.embed()
+
+randr.set_screen_size()
 
 for mode in res.modes:
     w, h = mode.width, mode.height
