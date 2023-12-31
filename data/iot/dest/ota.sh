d9c8dcc73da52a78eac88a1d3fdc2432e0d0a67b
Your Name <you@example.com>
2023-12-31 03:06:32 +0100

    ..

--- data/iot/dest/ota.sh
+++ data/iot/dest/ota.sh
@@ -4,6 +4,7 @@ sudo systemctl --now disable avahi-daemon.service
 
 
 docker run --rm --network host -v /var/run/dbus:/var/run/dbus -v /var/run/avahi-daemon/socket:/var/run/avahi-daemon/socket -v (pwd):/config  -it esphome/esphome run main.yaml --device 10.0.0.50 $argv
+# --device /dev/ttyUSB0 
 
 
 sudo systemctl --now enable avahi-daemon.service
