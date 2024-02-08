43d51c5367b1f76b828eacc002a49b0486f550b3
Jindrich Kolman <kolman.jindrich@gmail.com>
2024-02-08 13:10:29 +0100

    git_commit

--- data/setup/data/fish/functions/fish_prompt.fish
+++ data/setup/data/fish/functions/fish_prompt.fish
@@ -3,6 +3,19 @@ function fish_prompt --description 'Informative prompt'
     set -l last_pipestatus $pipestatus
     set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
 
+    set -l git_commit (git rev-parse --short HEAD)
+    if test -n "$git_commit"
+        echo -n (set_color green)"[$git_commit] "(set_color normal)
+    end
+
+# set -U __fish_git_prompt_show_informative_status 1
+# set -U __fish_git_prompt_showupstream "informative"
+# set -U __fish_git_prompt_showdirtystate "yes"
+# set -U __fish_git_prompt_showstashstate "yes"
+# set -U __fish_git_prompt_showuntrackedfiles "yes"\
+# set -U __fish_git_prompt_describe_style haha
+
+
     set -l vcs_info (fish_vcs_prompt)
 
     if functions -q fish_is_root_user; and fish_is_root_user
