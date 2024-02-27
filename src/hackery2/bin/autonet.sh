951f4e1c7a57a03a1e5a91bdf6bc26e5cdf3a110
Your Name <you@example.com>
2024-02-27 16:35:27 +0100

    ..

--- src/hackery2/bin/autonet.sh
+++ src/hackery2/bin/autonet.sh
@@ -6,10 +6,8 @@ sudo nmcli c show 'strasnice drat!' | grep GENERAL.STATE: | grep activated; and 
 sudo nmcli c show 'prosek drat'     | grep GENERAL.STATE: | grep activated; and exit 0;
 
 sudo dhclient -v enp0s25
-sudo dhcping -s    192.168.8.1   -h "a4:5d:36:9b:18:16";   and sudo nmcli c up 'strasnice drat!'
-sudo dhcping -s    10.0.0.138    -h "a4:5d:36:9b:18:16";   and sudo nmcli c up 'prosek drat'
 
-
-
+sudo dhcping -s  192.168.8.1   -h "a4:5d:36:9b:18:16";   and sudo nmcli c up 'strasnice drat!'; and sudo nmcli c down 'Not Mi Phone'
+sudo dhcping -s  10.0.0.138    -h "a4:5d:36:9b:18:16";   and sudo nmcli c up 'prosek drat'; and sudo nmcli c down 'Not Mi Phone'
 
 exit 0
