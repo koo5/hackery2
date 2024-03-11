5da0282a4b2f48a71982eb58072767dec848ed89
Your Name <you@example.com>
2024-03-11 14:42:39 +0100

    ..

--- src/hackery2/bin/autonet.sh
+++ src/hackery2/bin/autonet.sh
@@ -1,13 +1,28 @@
 #!/usr/bin/env fish
 
-#ping 8.8.8.8 -c 1; and exit 0
 
-sudo nmcli c show 'strasnice drat!' | grep GENERAL.STATE: | grep activated; and exit 0;
-sudo nmcli c show 'prosek drat'     | grep GENERAL.STATE: | grep activated; and exit 0;
+ping -c 20 8.8.8.8; or sudo dhclient -v enp0s25
 
-sudo dhclient -v enp0s25
 
-sudo dhcping -s  192.168.8.1   -h "a4:5d:36:9b:18:16";   and sudo nmcli c up 'strasnice drat!'; and sudo nmcli c down 'Not Mi Phone'
-sudo dhcping -s  10.0.0.138    -h "a4:5d:36:9b:18:16";   and sudo nmcli c up 'prosek drat'; and sudo nmcli c down 'Not Mi Phone'
+sudo dhcping -s  192.168.8.1   -h "a4:5d:36:9b:18:16"; and begin
+	sudo nmcli c show 'strasnice drat!' | grep GENERAL.STATE: | grep activated; 
+	and exit 0;
+	sudo nmcli c show 'xWLAN1-5G-FA4J68' | grep GENERAL.STATE: | grep activated;
+	and exit 0;
+	sudo nmcli c show 'xWLAN1-FA4J68' | grep GENERAL.STATE: | grep activated;
+	and exit 0;
+
+	sudo nmcli c up 'strasnice drat!';
+	and sudo nmcli c down 'Not Mi Phone';
+	and exit 0;
+end;
+
+sudo dhcping -s  10.0.0.138    -h "a4:5d:36:9b:18:16"; and begin
+	sudo nmcli c show 'prosek drat'         | grep GENERAL.STATE: | grep activated; 
+	and exit 0;
+	sudo nmcli c up 'prosek drat';
+	and sudo nmcli c down 'Not Mi Phone';
+	and exit 0;
+end;
 
 exit 0
