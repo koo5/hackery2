f51ee8a1b7efadab0d14036aabce0151d6e26993
Your Name <you@example.com>
2024-06-20 08:18:02 +0200

    ..

--- data/setup/data/fish/config.fish
+++ data/setup/data/fish/config.fish
@@ -2,6 +2,14 @@ if status is-interactive  # Commands to run in interactive sessions can go here
   . /home/koom/hackery2/data/setup/data/fish/functions/auto_venv.fish
   __auto_source_venv
   . /home/koom/hackery2/data/setup/data/fish/functions/aaaa.fish
+
+
+  abbr --add sa ssh-add
+abbr --add kw kwrite
+abbr --add untar tar -xvf
+abbr --add root sudo terminator
+
+
 end
 
 
