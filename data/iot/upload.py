50c0ab47b7f012360f0b22b4e76485270f134eb0
Your Name <you@example.com>
2024-08-16 22:00:39 +0200

    max6675 kinda sucks

--- data/iot/upload.py
+++ data/iot/upload.py
@@ -21,20 +21,23 @@ import jinja2
 app = typer.Typer()
 
 @app.command()
-def run(dir: pathlib.Path, bid=0, cmd='run', usb=''):
+def run(dir: pathlib.Path, bid=0, cmd='run', device=''):
 
 	# where to upload
 
-	if usb == 'auto':
+	if device == 'auto':
 		for tty in ["/dev/ttyUSB0", "/dev/ttyUSB1", "/dev/ttyACM0"]:
 			if os.path.exists(tty):
-				usb = tty
+				device = tty
 				break
-	
-	if usb != '':
-		usb = f'--device {usb}'			
-	
-		
+
+	usb = ''
+
+	if device != '':
+		if device.startswith('/dev/'):
+			usb = f'--device {device}'
+		device = f'--device {device}'
+
 	# what to upload
 
 	os.chdir(dir)
@@ -45,7 +48,18 @@ def run(dir: pathlib.Path, bid=0, cmd='run', usb=''):
 	
 	instdir = pathlib.Path(f'inst/{bid}')
 	os.makedirs(instdir, exist_ok=True)	
-	
+
+	config = {}
+	config['dir'] = name
+	config['bid'] = bid
+	config['dirbid'] = f'{name}{bid}'
+
+	config_file = pathlib.Path('config.py')
+	if config_file.exists():
+		sys.path.append('.')
+		import config as config_module
+		config = config_module.config(config)
+
 	yaml_files = list(pathlib.Path('.').glob('*.yaml'))
 	print(yaml_files)
 	for yaml_file in yaml_files:
@@ -53,7 +67,7 @@ def run(dir: pathlib.Path, bid=0, cmd='run', usb=''):
 			yaml = f.read()
 			step = jinja2.Template(yaml, autoescape=False, keep_trailing_newline=True)
 			#tmpl = step.Template(TEMPLATE_STRING, strip=False, escape=False)
-			yaml = step.render(dir = name, bid = bid, dirbid = f'{name}{bid}')
+			yaml = step.render(**config)
 		out = f'{instdir}/{yaml_file}'
 		with open(out, 'w') as f:
 			f.write(yaml)
@@ -76,7 +90,7 @@ def run(dir: pathlib.Path, bid=0, cmd='run', usb=''):
 	
 	os.chdir(instdir)
 	
-	cmd = f"docker run --rm --network host -v /var/run/dbus:/var/run/dbus -v (pwd):/config {usb} -it esphome/esphome -s name {name} {cmd} main.yaml {usb}"
+	cmd = f"docker run --rm --network host -v /var/run/dbus:/var/run/dbus -v (pwd):/config {usb} -it esphome/esphome -s name {name} {cmd} main.yaml {device}"
 	print(cmd)
 	os.system(f'fish -c "{cmd}"')
 	
---
+++ data/iot/viol/config.py
@@ -0,0 +1,4 @@
+def config(config):
+    config['ipbase'] = 120
+    return config
+
