3a7b03f1fd5c84821b7fa2de6016b9dd96687340
Jindrich Kolman <kolman.jindrich@gmail.com>
2024-09-05 20:12:15 +0200

    sa

--- src/hackery2/bin/fix_github_https_repo.sh
+++ src/hackery2/bin/fix_github_https_repo.sh
@@ -6,17 +6,17 @@ REMOTE_NAME="$1"
 REMOTE_NAME=${1-origin}
 echo "fixing $REMOTE_NAME ..."
 
-REPO_URL=`git remote -v | grep -m1 "^$REMOTE_NAME" | sed -Ene's#.*(https://[^[:space:]]*).*#\1#p'`
+REPO_URL=`git remote -v | grep -m1 "^$REMOTE_NAME" | sed -Ene's#.*(https?://[^[:space:]]*).*#\1#p'`
 if [ -z "$REPO_URL" ]; then
   echo "-- ERROR:  Could not identify Repo url."
   echo "   It is possible this repo is already using SSH instead of HTTPS."
   exit
 fi
 
-USER=`echo $REPO_URL | sed -Ene's#https://github.com/([^/]*)/(.*)(\.git)?#\1#p'`
+USER=`echo $REPO_URL | sed -Ene's#https?://github.com/([^/]*)/(.*)(\.git)?#\1#p'`
 HOST=github.com
 if [ -z "$USER" ]; then
-  USER=`echo $REPO_URL | sed -Ene's#https://gitlab.com/([^/]*)/(.*)(\.git)?#\1#p'`
+  USER=`echo $REPO_URL | sed -Ene's#https?://gitlab.com/([^/]*)/(.*)(\.git)?#\1#p'`
   HOST=gitlab.com
   if [ -z "$USER" ]; then
     echo "-- ERROR:  Could not identify User."
@@ -24,9 +24,9 @@ if [ -z "$USER" ]; then
   fi
 fi
 
-REPO=`echo $REPO_URL | sed -Ene's#https://github.com/([^/]*)/(.*)#\2#p'`
+REPO=`echo $REPO_URL | sed -Ene's#https?://github.com/([^/]*)/(.*)#\2#p'`
 if [ -z "$REPO" ]; then
-  REPO=`echo $REPO_URL | sed -Ene's#https://gitlab.com/([^/]*)/(.*)#\2#p'`
+  REPO=`echo $REPO_URL | sed -Ene's#https?://gitlab.com/([^/]*)/(.*)#\2#p'`
   if [ -z "$REPO" ]; then
     echo "-- ERROR:  Could not identify Repo."
     exit
