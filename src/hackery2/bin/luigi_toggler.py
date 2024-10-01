f696b7a5e3ddad3eba45ab7d353e173859a4742b
Your Name <you@example.com>
2024-10-01 22:03:40 +0200

    ....

--- src/hackery2/bin/luigi_toggler.py
+++ src/hackery2/bin/luigi_toggler.py
@@ -2,8 +2,22 @@
 
 import subprocess, requests
 
+# control luigi and core_control
+
 on = int(subprocess.check_output(['xprintidle'])) > 1000*60*2
 un = ('un' if on else '')
 cmd = f'http://localhost:8082/api/{un}pause'
 print(f'{cmd}')
-requests.get(cmd)
+try:
+    requests.get(cmd, timeout=3)
+except:
+    pass
+
+cores = 24 if on else 10
+cmd = 'http://192.168.122.128:1111/set_cores={}'.format(cores)
+print(f'{cmd}')
+try:
+    requests.get(cmd, timeout=3)
+except:
+    pass
+
