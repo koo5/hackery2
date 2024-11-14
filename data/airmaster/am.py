ff1c05e29d10e77d1c603507b4f5e5ecbf41d00f
Your Name <you@example.com>
2024-11-14 20:28:33 +0100

    ..

--- data/airmaster/am.py
+++ data/airmaster/am.py
@@ -2,14 +2,19 @@
 import glob
 import os, sys
 import subprocess
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
 
 usb_dev = os.environ.get('USB_DEV', None)
 if usb_dev is None:
-    print('USB_DEV not set')
+    log.error('USB_DEV not set')
     sys.exit(1)
 
 dev = glob.glob(usb_dev + '/ttyUSB*')
-print(dev)
+log.info(dev)
 name = dev[0].split('/')[-1]
 
 AM7_PORT = '/dev/' + name
@@ -19,6 +24,6 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 eee = os.environ.copy()
 eee['AM7_PORT'] = AM7_PORT
 
-print('AM7_PORT:', AM7_PORT)
+log.info('AM7_PORT: ' + AM7_PORT)
 
 sys.exit(subprocess.run(['./your_program_name'], env=eee).returncode)
