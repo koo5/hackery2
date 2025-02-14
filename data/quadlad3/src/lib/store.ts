1e46de919dce338c32cce08c4ebcd01e64b516a1
Jindrich Kolman <kolman.jindrich@gmail.com>
2025-02-15 00:19:35 +0100

    ..

--- data/quadlad3/src/lib/store.ts
+++ data/quadlad3/src/lib/store.ts
@@ -5,13 +5,13 @@ type ValMap = Map<Ref, Val>;
 
 class Quad {
     /* subject */
-    s: Ref;
+    s: Val;
     /* predicate/property/verb */
-    p: Ref;
+    p: Val;
     /* object */
-    o: Ref;
+    o: Val;
     /* unique identifier */
-    q: Ref;
+    q: Val;
 
     constructor(s, p, o, q) {
         this.s = s;
@@ -21,7 +21,9 @@ class Quad {
     }
 }
 
-/* big graphs are split into chunks, chunks are a unit of transmission and bookkeeping, transparent to the application. */
+/* big graphs are split into chunks, chunks are a unit of transmission and bookkeeping, transparent to the application.
+* gotta figure out if a chunk needs an identifier.
+*  */
 class Chunk {
     offset: number;
     graph: Ref;
@@ -30,11 +32,11 @@ class Chunk {
 
 
 /* a map of domain-specific materializations */
-type MatMap = Map<Ref, any>;
+type MatMap = Map<Val, any>;
 
 
 type IndexKey = Val | readonly [Val, Val] | readonly [Val, Val, Val] | readonly [Val, Val, Val, Val];
-type IndexedQuads = Map<IndexKey, Quad[]>;
+type IndexedQuads = Map<IndexKey, Quad | Quad[]>;
 
 enum IndexItem {
     s,
@@ -43,13 +45,13 @@ enum IndexItem {
     q
 }
 
-type IndexingStrategy = readonly [IndexItem] | readonly [IndexItem, IndexItem] | readonly [IndexItem, IndexItem, IndexItem] | readonly [IndexItem, IndexItem, IndexItem, IndexItem];
+type IndexingStrategy = IndexItem | readonly [IndexItem, IndexItem] | readonly [IndexItem, IndexItem, IndexItem] | readonly [IndexItem, IndexItem, IndexItem, IndexItem];
 
 class Store {
     /* a Map of Materialization Maps.  */
     mats: MatMap;
     /* a Mapping from Refs to actual values.*/
-    vals: ValMap;
+    //vals: ValMap;
     /* bookkeeping */
     chunks: Chunk[];
     /* indexes/quads */
@@ -57,40 +59,47 @@ class Store {
 
     constructor() {
         this.mats = new MatMap();
-        this.vals = new Map();
         this.chunks = [];
         this.iqs = new Map();
+        this._add_strategy(IndexItem.q);
     }
 
-    /* if we want to support non-hashable values:
-
-    add(s: any, p: any, o: any, q: any)
-    {
-        if (typeof s === 'object')
-            let ref = this.add_ref(s);
-            ....
-
-    gc()
-    {
-        ...
+    _add_strategy(strategy: IndexingStrategy) {
+        this.iqs.set(strategy, new Map());
+        /* todo: iterate quads from existing strategies and add to new strategy */
     }
-    */
 
-
-
-     */
-
-    add(s: Ref, p: Ref, o: Ref, q: Ref)
-    /* add a quad to the store. Add  */
-    {
+    add(s: Val, p: Val, o: Val, q: Val)
+    /* add a quad to the store. Add  */ {
         let quad = new Quad(s, p, o, q);
-        let iqs = this.iqs[[quad.q]]
-        if (iqs === undefined)
-        {
-            iqs = new Map();
-            this.iqs[[quad.q]] = iqs;
+        let iqs = this.iqs.get(IndexItem.q);
+        if (iqs === undefined) {
+            throw new Error("index not found");
         }
-        let key = [quad.q];
+        let key = quad.q;
+        /* q's are unique, so this.iqs[[quad.q]] maps to a single quad */
+        iqs.set(key, quad);
+        /* todo add to local chunk? */
+    }
+
+    *query(s: Val | null, p: Val | null, o: Val | null, q: Val | null) {
+        let iqs = this.iqs.get(IndexItem.q);
+        for (let [key, quad] of iqs) {
+            if (s !== null && quad.s !== s) {
+                continue;
+            }
+            if (p !== null && quad.p !== p) {
+                continue;
+            }
+            if (o !== null && quad.o !== o) {
+                continue;
+            }
+            if (q !== null && quad.q !== q) {
+                continue;
+            }
+            yield quad;
+        }
+    }
 
 
 
