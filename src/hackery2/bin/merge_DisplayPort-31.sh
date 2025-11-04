a9d38b335febfab1ac5859ca212adb3c5f348df0
Jindrich Kolman <kolman.jindrich@gmail.com>
2025-11-04 03:03:11 +0100

    ..

---
+++ src/hackery2/bin/merge_DisplayPort-31.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#randr --output DP-0 --above DisplayPort-1
+#leep 1;
+#randr --output DP-1 --above DisplayPort-2
+#leep 1;
+
+xrandr --setmonitor SomeName4 auto DisplayPort-3,DisplayPort-1
+
+#nohup xfwm4 --replace # disown?
+
+
+
+
